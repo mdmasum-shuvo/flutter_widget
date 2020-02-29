37d81beb50b1dc79ae6c83712a29e5adb8b18cf8
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2020-02-29 17:44:16 +0600

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,28 @@
 import 'package:flutter/material.dart';
+
 void main() {
-  runApp(SampleApp());
+  runApp(MaterialApp(
+    title: 'My App',
+    home: MyApp(),
+  ));
 }
 
-class SampleApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sample App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SampleAppPage(),
-    );
-  }
-}
-
-class SampleAppPage extends StatefulWidget {
-  SampleAppPage({Key key}) : super(key: key);
-
-  @override
-  _SampleAppPageState createState() => _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  // Default placeholder text
-  String textToShow = "I Like Flutter";
-
-  void _updateText() {
-    setState(() {
-      // update the text
-      textToShow = "Flutter is Awesome!";
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sample App"),
-      ),
-      body: Center(child: Text(textToShow)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _updateText,
-        tooltip: 'Update Text',
-        child: Icon(Icons.update),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Home'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+                'One of the best things about Android is just how customizable it is. Android phones let you personalize them to your heart’s content in order to improve the experience and express yourself. Changing fonts for Android seems like a logical extension of this philosophy, though it isn’t always as straightforward as you might expect.'
+                ' The good news is that where there is a will, there is a way! '
+                'Read on to learn how to change fonts for Android!',
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                    color: Colors.green, fontStyle: FontStyle.italic))));
   }
 }
